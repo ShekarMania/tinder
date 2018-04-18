a9b0473930a04bdfd0aaaf77b169dcc56a5ca6d1
ShekarMania <chandrasekhar.kohli@gmail.com>
2018-04-18 15:24:04 +0530

    Extended Swipe
    Extended Swipe Functionality to all the cards in the list and as soon as
    the first card gets swiped either direction, we gain the ability to
    swipe the next card and the next card comes to position 0,0.and the
    swiped cards are not rendered.But still all the cards are rendering as a
    list till now.

--- src/Deck.js
+++ src/Deck.js
@@ -7,8 +7,15 @@ import { View,
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH ;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component{
+
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {}
+  }
+
   constructor(props){
     super(props);
 
@@ -20,16 +27,34 @@ class Deck extends Component{
       },
       onPanResponderRelease:(event , gesture) => {
         if(gesture.dx > SWIPE_THRESHOLD){
-          console.log("Swiped RIght");
+          this.forceSwipe('right');
         }else if(gesture.dx < -SWIPE_THRESHOLD){
-          console.log("Swiped Left");
+          this.forceSwipe('left');
         }else{
-          this.resetPosition(); 
+          this.resetPosition();
         }
       }
     });
-    this.state = { panResponder , position };
+    this.state = { panResponder , position ,index :0};
   }
+
+  forceSwipe(direction){
+    const x = direction == 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+      Animated.timing(this.state.position, {
+        toValue: { x , y : 0},
+        duration : SWIPE_OUT_DURATION
+      }).start(() => this.onSwipeComplete(direction));
+  }
+
+  onSwipeComplete(direction){
+    const { onSwipeLeft , onSwipeRight , data } = this.props;
+    const item = data[this.state.index]
+    direction == 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+    //Post Swipe
+    this.state.position.setValue({x : 0 , y : 0});
+    this.setState({ index : this.state.index +1 });
+  }
+
 
   resetPosition(){
     Animated.spring(this.state.position, {
@@ -51,8 +76,11 @@ class Deck extends Component{
   }
 
   renderCards(){
-    return this.props.data.map((item , index) => {
-      if(index == 0){
+    return this.props.data.map((item , i) => {
+      if(i<this.state.index){
+        return null;
+      }
+      if(i == this.state.index){
         return(
           <Animated.View
             key={item.id}
